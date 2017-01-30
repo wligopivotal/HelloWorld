f34fbe55a8ad121755f01622de68fb1150ce4b04
wligopivotal <wli@gopivotal.com>
2017-01-30 11:04:33 +0800

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package test.com;
+
+/**
+ * Created by Administrator on 2017/1/29.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
